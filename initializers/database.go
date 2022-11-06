419219eed83663abb3e40fefe2d8f7cac25f90c7
Tony Starikov <tony.starikov.1992@gmail.com>
2022-11-06 23:50:27 +0200

    Add docker to project

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,16 @@ func ConnectToDatabase() {
 	var err error
 
 	// Elephantsql postgres test db
-	dsn := os.Getenv("DB_URL")
+	//dsn := os.Getenv("DB_URL")
+
+	// Docker db
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
